527006021336eda1213af6bfa254442f4cf6879e
sampada109 <sampadakaushik2518@gmail.com>
2024-05-01 13:32:36 +0530

    6th commit created data model for recipes

--- recipes/veges/models.py
+++ recipes/veges/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class recipes(models.Model):
+    recp_name = models.CharField(max_length=100)
+    recp_desp = models.TextField()
+    recp_img = models.ImageField(upload_to='recipe_img')
