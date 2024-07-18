0f17778ba14a5b248150c9ebbea62af0554769cf
sampada109 <sampadakaushik2518@gmail.com>
2024-07-18 18:54:42 +0530

    39th commit

--- recipes/recipes/settings.py
+++ recipes/recipes/settings.py
@@ -98,6 +98,8 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
+print(os.environ.get('DATABASE_URL'))
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
