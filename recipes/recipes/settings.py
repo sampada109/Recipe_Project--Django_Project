30f1d6225e8ac359c1301c7343a505b8c4403590
sampada109 <sampadakaushik2518@gmail.com>
2024-07-18 18:41:15 +0530

    37th commit - adding favicons and finishing

--- recipes/recipes/settings.py
+++ recipes/recipes/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-if DEBUG:
+if not DEBUG:
     STATICFILES_DIRS = [BASE_DIR / "static",] 
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
