b1d527ed3234001893ae200ab385e75f424c0baa
sampada109 <sampadakaushik2518@gmail.com>
2024-07-18 21:00:38 +0530

    41th commit - connecting to cloudinary to store media files

--- recipes/veges/models.py
+++ recipes/veges/models.py
@@ -20,7 +20,7 @@ class recipes(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)     #14th commit
     recp_name = models.CharField(max_length=100)
     recp_desp = models.TextField()
-    recp_img = models.ImageField(upload_to='recipe_img')
+    recp_img = models.ImageField(upload_to='recipe_img/')
     recp_create_date = models.DateTimeField(default=timezone.now)     #21
     recp_last_modified_date = models.DateTimeField(auto_now=True)
     tags = models.ManyToManyField(Tags) 
